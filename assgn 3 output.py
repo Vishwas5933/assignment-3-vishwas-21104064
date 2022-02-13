9ec39e5e542fca888114dd38c3bf4f545eb5f138
Vishwas5933 <97469817+Vishwas5933@users.noreply.github.com>
2022-02-14 00:00:29 +0530

    Add files via upload

---
+++ assgn 3 output.py
@@ -0,0 +1,107 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: C:/python3.10/assgn 3 ques 1.py ===================
+Question 1
+Please give an input- vishwas
+Total occurances
+v-1
+i-1
+s-2
+h-1
+w-1
+a-1
+
+=================== RESTART: C:/python3.10/assgn 3 ques 2.py ===================
+Question 2
+Please enter Day- 28
+Please enter Month- 01
+Please enter Year- 2003
+Next Date is: 29/1/2003
+
+=================== RESTART: C:/python3.10/assgn 3 ques 3.py ===================
+Question 3
+Plz enter Y to enter data- y
+Plz give an integer input- 3
+Plz enter Y to enter data- y
+Plz give an integer input- 6
+Plz enter Y to enter data- y
+Plz give an integer input- 7
+Plz enter Y to enter data- 5
+[3, 6, 7]
+[(3, 9), (6, 36), (7, 49)]
+
+=================== RESTART: C:/python3.10/assgn 3 ques 4.py ===================
+Question 4
+Enter Grade Points- 10
+Your Grade is A+ and Outstanding performance 
+
+=================== RESTART: C:/python3.10/assgn 3 ques 5.py ===================
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+=================== RESTART: C:\python3.10\assgn 3 ques 6.py ===================
+Question 6
+Please Enter SID of Student- 21104064
+Please enter the name of the Student- vishwas
+If you want to enter more details press Y/N- y
+Please Enter SID of Student- 21104017
+Please enter the name of the Student- shiven goyal
+If you want to enter more details press Y/N- y
+Please Enter SID of Student- 21104028
+Please enter the name of the Student- aditya kumar
+If you want to enter more details press Y/N- y
+Please Enter SID of Student- 21104029
+Please enter the name of the Student- arjun datta
+If you want to enter more details press Y/N- n
+Part a
+21104064 is vishwas
+21104017 is shiven goyal
+21104028 is aditya kumar
+21104029 is arjun datta
+
+Part b
+value sorted dictionary is ['aditya kumar', 'arjun datta', 'shiven goyal', 'vishwas']
+
+Part c
+Keys sorted dictionary is [21104017, 21104028, 21104029, 21104064]
+
+Part D
+Please enter the student's SID whose detail you need- 21104064
+Name of the required student is vishwas
+
+
+============================================================= RESTART: C:/python3.10/assgn 3 ques 7.py ============================================================
+Question 7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 10
+Fibonnaci sequence with 10 terms
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+Average of total 10 terms of sequence is 8.8
+END
+
+============================================================= RESTART: C:/python3.10/assgn 3 ques 8.py ============================================================
+Question 8
+Part a
+set with elements not common in both is {1, 3, 5, 6, 8}
+Part b
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+Part C
+Set of elements that is only present in two set is {1, 2, 4, 5}
+Part d
+set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3 {10, 7}
